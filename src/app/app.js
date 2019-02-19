50e60a4e22549d041a4df6a63c8f595959019807
Thai (Isaac) Nguyen <thai.nguyentran@pycogroup.com>
2019-02-19 17:08:38 +0700

    - Add babel's alias config - Add basic router's middleware

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,21 @@
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 
+import routes from '@app/routes';
 import { homeController } from '@app/controllers/home.controller';
 
 const app = express();
 dotenv.config();
 
 app.set('port', process.env.PORT || 3000);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/api', routes);
+
 app.get('/', homeController);
 
 export default app;
